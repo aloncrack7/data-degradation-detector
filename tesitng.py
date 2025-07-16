7bf9fed0a8309a215dc5683b5082f26cbb23d9fb
aloncrack7 <alonmadrid@gmail.com>
2025-07-16 12:33:51 -0500

    Remove: testing file

--- tesitng.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from data_degradation_detector import univariate as uv
-import numpy as np
-
-df = pd.read_csv("WineQT.csv")
-
-dfs = np.array_split(df, 10)
-uv.descriptor_evolution_from_dfs(dfs, 'fixed acidity')
